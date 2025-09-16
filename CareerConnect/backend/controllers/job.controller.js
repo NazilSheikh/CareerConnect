0377f03f742a0a88bd01186e0ef363c119c7f664
Nazil Sheikh <nazilsheikh84@gmail.com>
2025-09-16 21:23:49 +0530

    Initial commit: Add fullstack job portal project

---
+++ CareerConnect/backend/controllers/job.controller.js
@@ -0,0 +1,109 @@
+import { Job } from "../models/job.model.js";
+
+// admin post krega job
+export const postJob = async (req, res) => {
+    try {
+        const { title, description, requirements, salary, location, jobType, experience, position, companyId } = req.body;
+        const userId = req.id;
+
+        if (!title || !description || !requirements || !salary || !location || !jobType || !experience || !position || !companyId) {
+            return res.status(400).json({
+                message: "Somethin is missing.",
+                success: false
+            })
+        };
+        const job = await Job.create({
+            title,
+            description,
+            requirements: requirements.split(","),
+            salary: Number(salary),
+            location,
+            jobType,
+            experienceLevel: experience,
+            position,
+            company: companyId,
+            created_by: userId
+        });
+        return res.status(201).json({
+            message: "New job created successfully.",
+            job,
+            success: true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+// student k liye
+export const getAllJobs = async (req, res) => {
+    try {
+        // getting job by filter keyword
+        const keyword = req.query.keyword || "";
+        // i will assume small and upper as same character 
+        const query = {
+            $or: [
+                { title: { $regex: keyword, $options: "i" } },
+                { description: { $regex: keyword, $options: "i" } },
+            ]
+        };
+        const jobs = await Job.find(query).populate({
+            path: "company"
+        }).sort({ createdAt: -1 });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+// student jobs find 
+export const getJobById = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path:"applications"
+        });
+        if (!job) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({ job, success: true });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// ADMIN KE LIEY 
+
+
+// admin kitne job create kra hai abhi tk
+export const getAdminJobs = async (req, res) => {
+    try {
+        const adminId = req.id;
+        // admin id will give admins jobs to paticular user // populate will give me comapny information not its id 
+        const jobs = await Job.find({ created_by: adminId }).populate({
+            path:'company',
+            createdAt:-1
+        });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
